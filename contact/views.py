f82bb201351e3178421a5fe303270cfb10323689
Konrad Jopek <kj@Slappy.(none)>
2012-06-07 02:50:54 +0200

    initial views support

--- contact/views.py
+++ contact/views.py
@@ -1,1 +1,11 @@
+# -*- coding: utf-8 -8-
+
 # Create your views here.
+
+def index(request):
+    # TODO 
+    return ''
+
+def send(request):
+    # TODO
+    return ''
--- zubird/urls.py
+++ zubird/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import patterns, include, url
-
 # Uncomment the next two lines to enable the admin:
 from django.contrib.gis import admin
 admin.autodiscover()
@@ -14,4 +13,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^send/', 'contact.views.send'),
+    url(r'^/', 'contact.views.index'),
+    
 )
